ab3106bb33bb0bc28f65caab40a96394cf37bf63
roj21008 <roj21008@byui.edu>
2024-11-23 12:08:06 -0500

    CHAPTER 12

--- nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -1,24 +1,21 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
-import {
-  CheckIcon,
-  ClockIcon,
-  CurrencyDollarIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
+import { CheckIcon, ClockIcon, CurrencyDollarIcon,  UserCircleIcon,} from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
 
-export default function EditInvoiceForm({
+export default function EditInvoiceForm({ 
   invoice,
   customers,
-}: {
+  }:{
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  return (
-    <form>
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  
+  return (<form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -119,5 +116,6 @@ export default function EditInvoiceForm({
         <Button type="submit">Edit Invoice</Button>
       </div>
     </form>
-  );
+  
+)  
 }
